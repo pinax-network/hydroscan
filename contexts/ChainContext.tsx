f5363afbd4323b38c6bb1b9f8055459a6c2cf925
Denis Carriere <carriere.denis@gmail.com>
2025-12-09 12:55:19 -0500

    Add USDT+USDC tokens

--- contexts/ChainContext.tsx
+++ contexts/ChainContext.tsx
@@ -19,6 +19,29 @@ const tokens: Record<string, { symbol: string; contract: string }[]> = {
     [TVMChains.Tron]: [
         { symbol: "USDT", contract: "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t" },
     ],
+    [EVMChains.ArbitrumOne]: [
+        { symbol: "USD₮0", contract: "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9" },
+        { symbol: "USDC", contract: "0xaf88d065e77c8cc2239327c5edb3a432268e5831" },
+        { symbol: "USDC.e", contract: "0xff970a61a04b1ca14834a43f5de4533ebddb5cc8" },
+    ],
+    [EVMChains.Polygon]: [
+        { symbol: "USD₮0", contract: "0xc2132d05d31c914a87c6611c10748aeb04b58e8f" },
+        { symbol: "USDC", contract: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359" },
+        { symbol: "USDC.e", contract: "0x2791bca1f2de4661ed88a30c99a7a9449aa84174" },
+    ],
+    [EVMChains.Avalanche]: [
+        { symbol: "USDT", contract: "0x9702230A8Ea53601f5cD2dc00fDBc13d4dF4A8c7" },
+        { symbol: "USDC", contract: "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E" },
+        { symbol: "USDC.e", contract: "0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664" },
+    ],
+    [EVMChains.Unichain]: [
+        { symbol: "USD₮0", contract: "0x9151434b16b9763660705744891fA906F660EcC5" },
+        { symbol: "USDC", contract: "0x078D782b760474a361dDA0AF3839290b0EF57AD6" },
+    ],
+    [EVMChains.Optimism]: [
+        { symbol: "USDT", contract: "0x94b008aa00579c1307b0ef2c499ad98a8ce58e58" },
+        { symbol: "USDC", contract: "0x0b2c639c533813f4aa9d7837caf62653d097ff85" },
+    ],
 };
 
 interface ChainContextType {
